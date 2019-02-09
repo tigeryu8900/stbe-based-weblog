d084d16ae4ccdfba18811122caaee488ce9f38ed
tigeryu8900 <tigeryu8900@gmail.com>
2019-02-09 21:20:01 +0800

    update re2.cmake 	modified:   external/re2.cmake

--- external/re2.cmake
+++ external/re2.cmake
@@ -1,38 +1,49 @@
 include (ExternalProject)
 
-set(re2_INCLUDE_DIR ${CMAKE_CURRENT_BINARY_DIR}/re2/install/include)
-set(re2_URL https://github.com/google/re2)
-set(re2_BUILD ${CMAKE_CURRENT_BINARY_DIR}/re2/src/re2)
-set(re2_INSTALL ${CMAKE_CURRENT_BINARY_DIR}/re2/install)
-set(re2_TAG e7efc48)
+set(re2_INCLUDE_DIR ${CMAKE_CURRENT_BINARY_DIR}/re2/src/re2/re2)
+set(re2_EXTRA_INCLUDE_DIR ${CMAKE_CURRENT_BINARY_DIR}/re2/src/re2)
+set(re2_URL https://github.com/google/re2.git)
+set(re2_TAG master)
+set(re2_BUILD ${CMAKE_BINARY_DIR}/re2/src/re2)
+set(re2_LIBRARIES ${re2_BUILD}/obj/so/libre2.so)
+get_filename_component(re2_STATIC_LIBRARIES ${re2_BUILD}/libre2.a ABSOLUTE)
+set(re2_INCLUDES ${re2_BUILD})
 
-if(WIN32)
-  if(${CMAKE_GENERATOR} MATCHES "Visual Studio.*")
-    set(re2_STATIC_LIBRARIES ${re2_BUILD}/$(Configuration)/re2.lib)
-  else()
-    set(re2_STATIC_LIBRARIES ${re2_BUILD}/re2.lib)
-  endif()
-else()
-  set(re2_STATIC_LIBRARIES ${re2_BUILD}/libre2.a)
-endif()
-
+# We only need re2.h in external/re2/re2/re2.h
+# For the rest, we'll just add the build dir as an include dir.
 set(re2_HEADERS
-    ${re2_INSTALL}/include/re2/re2.h
+    "${re2_BUILD}/re2/re2.h"
 )
 
 ExternalProject_Add(re2
     PREFIX re2
     GIT_REPOSITORY ${re2_URL}
     GIT_TAG ${re2_TAG}
-    INSTALL_DIR ${re2_INSTALL}
-    BUILD_IN_SOURCE 1
-    BUILD_BYPRODUCTS ${re2_STATIC_LIBRARIES}
     DOWNLOAD_DIR "${DOWNLOAD_LOCATION}"
+    BUILD_IN_SOURCE 1
+    INSTALL_COMMAND ""
     CMAKE_CACHE_ARGS
         -DCMAKE_BUILD_TYPE:STRING=Release
-        -DCMAKE_INSTALL_PREFIX:STRING=${re2_INSTALL}
-        -DRE2_BUILD_TESTING:BOOL=OFF
+        -DCMAKE_VERBOSE_MAKEFILE:BOOL=OFF
 )
+
+## put re2 includes in the directory where they are expected
+add_custom_target(re2_create_destination_dir
+    COMMAND ${CMAKE_COMMAND} -E make_directory ${re2_INCLUDE_DIR}
+    DEPENDS re2)
+
+add_custom_target(re2_copy_headers_to_destination
+    DEPENDS re2_create_destination_dir)
+
+foreach(header_file ${re2_HEADERS})
+    add_custom_command(TARGET re2_copy_headers_to_destination PRE_BUILD
+    COMMAND ${CMAKE_COMMAND} -E copy ${header_file} ${re2_INCLUDE_DIR})
+endforeach()
+
+ADD_LIBRARY(re2_lib STATIC IMPORTED
+    DEPENDS re2)
+SET_TARGET_PROPERTIES(re2_lib PROPERTIES
+    IMPORTED_LOCATION ${re2_STATIC_LIBRARIES})
 
 message( "===============tigerdebug re2 include ${re2_INCLUDE_DIR}" )
 message( "===============tigerdebug re2 lib ${re2_STATIC_LIBRARIES}" )
